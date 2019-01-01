ee86476960108a8a8c5bc87858042df437ed4709
Adam Makboul <Adammak9810@gmail.com>
2019-01-01 12:40:49 -0500

    render

--- build/oobies/methods.js
+++ build/oobies/methods.js
@@ -1,15 +1,65 @@
+var faceState = {
+    faceColor: '#ffff00',
+    bgColor: '#ffffff',
+    faceWidth: 600,
+    faceHeight: 600
+};
+
 function setFaceColor(color) {
-    var $smiley = document.getElementById('smiley');
-    $smiley.style.backgroundColor = color; 
+    faceState.faceColor = color;
+    render();
 }
 
 function setBackgroundColor(color) {
-    document.getElementById('body').style.backgroundColor = color;
+    faceState.bgColor = color;
+    render();
 }
 
 function setFaceSize(size) {
-    var $smiley = document.getElementById('smiley');
-    $smiley.style.width = size + 'px';
-    $smiley.style.height = size + 'px';
-    $smiley.style.borderRadius = size/2 + 'px';
+    faceState.faceWidth = size;
+    faceState.faceHeight = size;
+    render();
 }
+
+function render() {
+    // update face color
+    var $smiley = document.getElementById('smiley');
+    $smiley.style.backgroundColor = faceState.faceColor; 
+
+    // update background color
+    document.getElementById('body').style.backgroundColor = faceState.bgColor;
+
+    // update face size
+    var $smiley = document.getElementById('smiley');
+    $smiley.style.width = faceState.faceWidth + 'px';
+    $smiley.style.height = faceState.faceHeight + 'px';
+    $smiley.style.borderRadius = faceState.faceWidth/2 + 'px';
+
+    // eyes common
+    var eyeWidth = faceState.faceWidth/10;
+    var eyeHeight = faceState.faceHeight/5;
+    var eyeBorder = faceState.faceWidth/20;
+    var eyePositionTop = faceState.faceHeight/5 + 'px';
+
+    // update left eye
+    var $leftEye = document.getElementById('leftEye');
+    $leftEye.style.width = eyeWidth + 'px';
+    $leftEye.style.height = eyeHeight + 'px';
+    $leftEye.style.borderRadius = eyeBorder + 'px';
+    $leftEye.style.top = eyePositionTop;
+    $leftEye.style.left = (faceState.faceWidth/3 - eyeWidth/2) + 'px';
+
+
+    // update right eye
+    var $rightEye = document.getElementById('rightEye');
+    $rightEye.style.width = eyeWidth + 'px';
+    $rightEye.style.height = eyeHeight + 'px';
+    $rightEye.style.borderRadius = eyeBorder + 'px';
+    $rightEye.style.top = eyePositionTop;
+    $rightEye.style.left = (2*faceState.faceWidth/3 - eyeWidth/2) + 'px';
+
+}
+
+window.onload = function() {
+    render();
+};
